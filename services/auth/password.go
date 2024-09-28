277eeb368fcfd65d970831a35a2c4e1143d2fd85
phildehovre <ph.dehovre@gmail.com>
2024-09-28 15:40:16 +0100

    getUserByEmail

--- services/auth/password.go
+++ services/auth/password.go
@@ -1,6 +1,10 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
@@ -12,5 +16,6 @@ func HashPassword(password string) (string, error) {
 
 func ComparePasswords(hashed string, plain []byte) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hashed), plain)
+	fmt.Println(err)
 	return err == nil
 }
